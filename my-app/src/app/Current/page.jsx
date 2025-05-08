aae04ebb0ccb7878148a0903e8326a471c1f757a
Klasterov <111446275+Klasterov@users.noreply.github.com>
2025-05-08 09:15:05 +0300

    Update page.jsx

--- my-app/src/app/Current/page.jsx
+++ my-app/src/app/Current/page.jsx
@@ -2,8 +2,8 @@
 'use client'
 import React from "react";
 import { Container } from "react-bootstrap";
-import { Select } from "@mui/material/";
-import { MenuItem } from "@mui/material/";
+import { Select } from "@mui/material/Select";
+import { MenuItem } from "@mui/material/MenuItem";
 import "./current.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
